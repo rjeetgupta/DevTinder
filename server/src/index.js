e90ef27945368f8879d618554f7ed6c08e5e3063
Ranjeet Gupta <ranjeeturd@gmail.com>
2025-11-17 13:36:09 +0530

    Project initialize and setup

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+
+dotenv.config({
+    path: "./.env"
+});
+
+const port = process.env.PORT || 4000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log("Server connection error !!", error)
+        process.exit(1)
+    }
+}
+
+startServer();
