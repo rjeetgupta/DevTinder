8f0d93c0a906c552b6a2eb5e1e2e12c3dea278ad
Ranjeet Gupta <ranjeeturd@gmail.com>
2025-11-25 16:51:34 +0530

    Setup routing for the applicaiton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+import AppLayout from "./components/AppLayout"
 
 
 
@@ -5,10 +7,12 @@ function App() {
 
 
   return (
-    <button
-      class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-      Button
-    </button>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
